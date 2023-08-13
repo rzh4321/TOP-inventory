96afe8f1f0510b681c650c0252c72fecc2e85f25
Ricky <rzh4321@gmail.com>
2023-08-13 18:01:14 -0400

    - pages display "None" if fields are empty - created createTeam and createPosition forms

--- routes/positions.js
+++ routes/positions.js
@@ -12,6 +12,10 @@ router.get('/', asyncHandler(async (req, res, next) => {
   res.render("positionList", {title: "Positions List", positionList});
 }));
 
+router.get('/create', asyncHandler(async (req, res, next) => {
+  res.render("positionCreate", {title: "Create position"});
+}));
+
 router.get('/:id', asyncHandler(async (req, res, next) => {
   const [position, players] = await Promise.all([
     Position.findById(req.params.id),
@@ -19,5 +23,25 @@ router.get('/:id', asyncHandler(async (req, res, next) => {
   ])
   res.render("positionDetails", {title: position.position, players, position})
 }));
+
+router.post('/create', [
+  body("position", "Position must be at least 2 characters")
+    .trim()
+    .isLength({min: 2})
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const position = new Position({position: req.body.position});
+    if (!errors.isEmpty()) {
+      res.render("positionCreate", {title: "Create position", position, errors: errors.array()});
+    }
+    else {
+      await position.save();
+      res.redirect(position.url);
+    }
+  })
+]);
+
 
 module.exports = router;
--- routes/teams.js
+++ routes/teams.js
@@ -14,6 +14,11 @@ router.get('/', asyncHandler(async (req, res, next) => {
   res.render("teamList", {title: "Teams List", teamList});
 }));
 
+router.get('/create', asyncHandler(async (req, res, next) => {
+  const teamList = await Team.find();
+  res.render("teamCreate", {title: "Create Team", teamList});
+}));
+
 router.get('/:id', asyncHandler(async (req, res, next) => {
   const [team, players, owners] = await Promise.all([
     Team.findById(req.params.id),
@@ -23,4 +28,37 @@ router.get('/:id', asyncHandler(async (req, res, next) => {
   res.render("teamDetails", {title: team.teamName, team, players, owners})
 }));
 
+router.post('/create', [
+  body("location", "Location cannot be empty")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body("name", "Name cannot be empty")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body("championships")
+    .custom(value => {
+      if (!value) return true;
+      const yearsList = value.split(' ').map(Number);
+      const currentYear = new Date().getFullYear();
+      for (const year of yearsList) {
+        if (year < 1900 || year > currentYear) return false;
+      }
+      return true;
+    }).withMessage("Invalid year format or range"),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const team = new Team({location: req.body.location, name: req.body.name, 
+      championships: req.body.championships === ''? [] : req.body.championships.split(' ').map(Number)});
+    if (!errors.isEmpty()) {
+      res.render("teamCreate", {title: "Create team", team, errors: errors.array()});
+    }
+    else {
+      await team.save();
+      res.redirect(team.url);
+    }
+  })
+]);
 module.exports = router;
